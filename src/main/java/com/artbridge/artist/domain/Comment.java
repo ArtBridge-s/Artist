23b5cf700ea83a652ce2c34a38e796be16c911d0
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-18 10:58:44 +0900

    refactor: Refactor Comment class in the domain package to use Lombok for getter/setter methods, simplify annotations, and remove unnecessary code

--- src/main/java/com/artbridge/artist/domain/Comment.java
+++ src/main/java/com/artbridge/artist/domain/Comment.java
@@ -1,10 +1,14 @@
 package com.artbridge.artist.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.io.Serializable;
-import javax.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * A Comment.
@@ -13,6 +17,10 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Table(name = "comment")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @SuppressWarnings("common-java:DuplicatedBlocks")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Comment implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -33,23 +41,13 @@ public class Comment implements Serializable {
     @JsonIgnoreProperties(value = { "comments", "views", "likes" }, allowSetters = true)
     private Artist artwork;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here
 
-    public Long getId() {
-        return this.id;
-    }
+
+
 
     public Comment id(Long id) {
         this.setId(id);
         return this;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getVoMember() {
-        return this.voMember;
     }
 
     public Comment voMember(Long voMember) {
@@ -57,29 +55,9 @@ public class Comment implements Serializable {
         return this;
     }
 
-    public void setVoMember(Long voMember) {
-        this.voMember = voMember;
-    }
-
-    public String getContent() {
-        return this.content;
-    }
-
     public Comment content(String content) {
         this.setContent(content);
         return this;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Artist getArtwork() {
-        return this.artwork;
-    }
-
-    public void setArtwork(Artist artist) {
-        this.artwork = artist;
     }
 
     public Comment artwork(Artist artist) {
@@ -87,32 +65,17 @@ public class Comment implements Serializable {
         return this;
     }
 
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Comment)) {
-            return false;
-        }
-        return id != null && id.equals(((Comment) o).id);
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Comment comment = (Comment) o;
+        return getId() != null && Objects.equals(getId(), comment.getId());
     }
 
     @Override
     public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
         return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Comment{" +
-            "id=" + getId() +
-            ", voMember=" + getVoMember() +
-            ", content='" + getContent() + "'" +
-            "}";
     }
 }
