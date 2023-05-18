3dcdd916759e25a1c283d907a6e46ad83ea5fe00
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-18 10:55:19 +0900

    refactor: Refactor ViewDTO class to use Lombok for getter/setter methods and remove unnecessary annotations and methods

--- src/main/java/com/artbridge/artist/service/dto/ViewDTO.java
+++ src/main/java/com/artbridge/artist/service/dto/ViewDTO.java
@@ -1,72 +1,18 @@
 package com.artbridge.artist.service.dto;
 
+import lombok.Data;
+
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * A DTO for the {@link com.artbridge.artist.domain.View} entity.
  */
 @SuppressWarnings("common-java:DuplicatedBlocks")
+@Data
 public class ViewDTO implements Serializable {
 
     private Long id;
-
     private Long voMember;
-
     private ArtistDTO artwork;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getVoMember() {
-        return voMember;
-    }
-
-    public void setVoMember(Long voMember) {
-        this.voMember = voMember;
-    }
-
-    public ArtistDTO getArtwork() {
-        return artwork;
-    }
-
-    public void setArtwork(ArtistDTO artwork) {
-        this.artwork = artwork;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ViewDTO)) {
-            return false;
-        }
-
-        ViewDTO viewDTO = (ViewDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, viewDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "ViewDTO{" +
-            "id=" + getId() +
-            ", voMember=" + getVoMember() +
-            ", artwork=" + getArtwork() +
-            "}";
-    }
 }
