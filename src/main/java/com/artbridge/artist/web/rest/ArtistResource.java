18f9b6bbb4b7a31857914c94f06fb96f4f9b8080
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-25 19:28:06 +0900

    docs(INVM-11): Add documentation for API endpoint to get delete pending artists

--- src/main/java/com/artbridge/artist/web/rest/ArtistResource.java
+++ src/main/java/com/artbridge/artist/web/rest/ArtistResource.java
@@ -197,6 +197,12 @@ public class ArtistResource {
     }
 
 
+    /**
+     * {@code GET /artists/pending/deletes} : 관리자 권한이 있는 경우 삭제 대기 중인 Artist 목록을 페이지별로 조회합니다.
+     *
+     * @param pageable 페이지 정보 (Pageable)
+     * @return 페이지별로 조회된 삭제 대기 중인 Artist 목록을 담은 ResponseEntity
+     */
     @GetMapping("/artists/pending/deletes")
     @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.ADMIN + "\")")
     public ResponseEntity<List<ArtistDTO>> getDeletePendings(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
