a02d1cbab03bd35c28dedb7ce31cd09849354f82
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-26 11:49:29 +0900

    feat(INYM-20): Add additional methods to CommentRepository

--- src/main/java/com/artbridge/artist/repository/CommentRepository.java
+++ src/main/java/com/artbridge/artist/repository/CommentRepository.java
@@ -1,6 +1,9 @@
 package com.artbridge.artist.repository;
 
 import com.artbridge.artist.domain.Comment;
+import com.hazelcast.jet.Traverser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,8 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {}
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    boolean existsByArtist_Id(Long artistId);
+
+    Page<Comment> findByArtist_Id(Pageable pageable, Long artistId);
+}
