2d5b305e6a21bd73c1b0dbcef00661215902c83e
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-01 10:46:37 +0900

    refactor(INVM-19): Improve MemberProducer implementation
    This commit refactors the MemberProducerImpl class. The changes include using the KafkaTemplate instead of KafkaProducer for producing Kafka messages, updating the logic to send a serialized MemberNameDTO object as the message payload, and handling exceptions appropriately. These improvements enhance the readability, maintainability, and error handling of the MemberProducer implementation.

--- src/main/java/com/artbridge/artist/adapter/MemberProducerImpl.java
+++ src/main/java/com/artbridge/artist/adapter/MemberProducerImpl.java
@@ -2,50 +2,40 @@ package com.artbridge.artist.adapter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Service
 @Slf4j
-public class MemberProducerImpl implements MemberProducer{
-
+public class MemberProducerImpl implements MemberProducer {
+    /*TODO: - need refactoring*/
 
     // 토픽명
     private static final String TOPIC_MEMBERNAME = "topic_membername";
 
-    private final KafkaProperties kafkaProperties;
-    private KafkaProducer<String, String> producer;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public MemberProducerImpl(KafkaProperties kafkaProperties) {
-        this.kafkaProperties = kafkaProperties;
+    public MemberProducerImpl(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    @PostConstruct
-    public void initialize() {
-        // KafkaProducer 객체 생성
-        log.info("Kafka Producer Initializing...");
-        this.producer = new KafkaProducer<>(kafkaProperties.buildProducerProperties());
-        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
-        log.info("Kafka Producer Initialized");
-    }
 
 
     @Override
     public void requestMemberName(Long id) {
-        // KafkaProducer 객체를 이용하여 토픽에 메시지 전송
-        log.info("Request MemberName to Kafka Producer");
-        producer.send(new ProducerRecord<>(TOPIC_MEMBERNAME, id.toString()));
-    }
+        MemberNameDTO memberNameDTO = new MemberNameDTO(id);
+        String message = null;
+        try {
+            message = objectMapper.writeValueAsString(memberNameDTO);
+            log.info("Request MemberName to Kafka Producer: {}", message);
+            kafkaTemplate.send(new ProducerRecord<>(TOPIC_MEMBERNAME, message)).get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Request MemberName to Kafka Producer Success: {}", message);
 
-    @PreDestroy
-    public void shutdown() {
-        log.info("Shutdown Kafka Producer...");
-        producer.close();
     }
 
 }
